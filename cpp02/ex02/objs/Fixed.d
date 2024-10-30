662b7ac35dc7a8d6cfb6ae74b3764d00aa14be8f
lydid0u <lydiaaboudjelal@gmail.com>
2024-10-30 20:45:15 +0100

    reset home

--- cpp02/ex02/objs/Fixed.d
+++
@@ -1,3 +0,0 @@
-objs/Fixed.o: srcs/Fixed.cpp srcs/../include/Fixed.hpp
-
-srcs/../include/Fixed.hpp:
--- cpp02/ex02/objs/main.d
+++
@@ -1,3 +0,0 @@
-objs/main.o: srcs/main.cpp srcs/../include/Fixed.hpp
-
-srcs/../include/Fixed.hpp:
