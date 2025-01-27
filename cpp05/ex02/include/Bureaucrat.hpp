ce84445310b2b8f29a6f660ea254eacb1306ce7f
lydid0u <lydiaaboudjelal@gmail.com>
2025-01-27 20:32:59 +0100

    cpp05 ex01

---
+++ cpp05/ex02/include/Bureaucrat.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lboudjel <lboudjel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 22:09:08 by lboudjel          #+#    #+#             */
+/*   Updated: 2025/01/25 18:03:13 by lboudjel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+#include <exception> 
+
+#define RESET   "\033[0m"
+#define RED	 "\033[31m"
+#define BLUE	"\033[34m" 
+#define YELLOW  "\033[33m"
+#define GREEN   "\033[32m" 
+
+class Form;
+
+class Bureaucrat {
+public:
+	Bureaucrat();
+    Bureaucrat(std::string name, int grade);
+    Bureaucrat(const Bureaucrat &other);
+    Bureaucrat &operator=(const Bureaucrat &rhs);
+	~Bureaucrat();
+
+	std::string getName() const ;
+	void		setGrade(int grade);
+	int 		getGrade() const ;
+
+	void			increment();
+	void			decrement();
+
+    void        signForm(Form &instance_form);
+    
+    class GradeTooHighException : public std::exception {
+    public:
+        virtual const char* what() const throw();
+    };
+    class GradeTooLowException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+    };
+
+
+private:
+	const std::string _name;
+	int	_grade;
+};
+
+std::ostream	&operator<<(std::ostream &out, const Bureaucrat &bureaucrat_instance);
+
+#endif // Bureaucrat_HPP
