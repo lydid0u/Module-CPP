62a8d9909c71b9f49666f2e104ab91bb05625a7e
lydidou - home <lydidou@DESKTOP-UI88K8Q.>
2025-02-19 01:58:22 +0100

    cpp09 ex01

--- cpp09/ex01/srcs/main.cpp
+++ cpp09/ex01/srcs/main.cpp
@@ -16,6 +16,8 @@
 #include <string.h>
 #include <iostream>
 #include <string>
+#include <cstdio>
+
 
 // Format dans fichier csv -> YYYY-MM-DD,Value = 2021-09-21,42901.56
 // Format dans infile date | value = 2011-01-03 | 3
@@ -28,8 +30,10 @@
 // main.cpp
 std::stack<char> mystack;
 
-int calc(char v1, char v2, char c)
+int calc(int v1, int v2, char c)
 {
+	std::cout << IYELLOW << "v1 : " << v1 << " v2 : " << v2 << RESET <<  std::endl;
+
 	if (c == '*')
 		return (v1 * v2);
 	if (c == '+')
@@ -38,19 +42,22 @@ int calc(char v1, char v2, char c)
 		return (v1 - v2);
 	if (c == '/' && v2 != 0)
 		return (v1 / v2);
+	std::cout << "testtt" << std::endl;
 	throw "ntm";
 }
 
 int function()
 {
-	int total = 0;
-	char v1, v2, op;
+	int v1, v2, total;
+	char op;
+	total = 0;
 	int size = mystack.size();
 	v1 = mystack.top();
 	mystack.pop();
+
 	if (mystack.empty())
 		return (total);
-	for (; mystack.size() > 0;)
+	for (int i = 0; mystack.size() > 0; i++)
 	{
 		if (size - 1 != mystack.size())
 			v1 = total;
@@ -65,39 +72,53 @@ int function()
 		else
 		{
 			v2 = mystack.top();
+			std::cout << "v2 : " << v2 << std::endl;
 			mystack.pop();
 			if (mystack.empty())
 				return (total);
 			op = mystack.top();
+			std::cout << "op : " << op << std::endl;
 		}
 		mystack.pop();
 		if (mystack.empty())
 			return (total);
-		total += calc(v1 - '0', v2  - '0', op);
-		std::cout << "v1 -> ," << v1 << std::endl;
-		std::cout << "v2 -> ," << v2 << std::endl;
-		std::cout << "op -> ," << op << std::endl;
-		std::cout << "total -> ," << total << std::endl;
+		total = calc(v1, v2 , op);
+		std::cout << "test" << std::endl;
+		std::cout << "v1 -> " << v1 << std::endl;
+		std::cout << "v2 -> " << v2 << std::endl;
+		std::cout << "op -> " << op << std::endl;
 	}
+	std::cout << "total -> " << total << std::endl;
 	return (total);
 }
+
 int main(int argc, char **argv)
 {
-	std::string line;
-
-	if (argc < 2)
-		return (1);
-	for (int i = argc - 1; i > 0; i--)
-	{
-		if (argv[i][1])
-			return (1);
-		mystack.push((argv[i][0]));
-	}
 	try {
-		std::cout << function() << std::endl;
+		if (argc < 2)
+			return (1);
+		for (int i = argc - 1; i > 0; i--)
+		{
+			if (argv[i][1])
+				return (1);
+			if (argv[i][0] >= '0' && argv[i][0] <= '9') {
+				argv[i][0] = argv[i][0] - '0';
+				mystack.push((argv[i][0]));
+			}
+			else if (strchr("+-/*", argv[i][0]))
+				mystack.push((argv[i][0]));
+			else
+				throw "not a digit or an operator";
+		}
+		try {
+			std::cout << function() << std::endl;
+		}
+		catch (std::exception &e) {
+			std::cerr << "ca a foire: " << e.what() << std::endl;
+		}
 	}
 	catch (std::exception &e) {
-		std::cerr << "ca a foire: " << e.what() << std::endl;
+		std::cerr << "ca a totalement foire: " << e.what() << std::endl;
 	}
 
 	return (0);
